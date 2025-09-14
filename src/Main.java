f7c31e73cb38ba3e5b9c6786037605dbc1610c9b
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-09-14 15:45:14 -0400

    Add files via upload
    Missing curly brace on line 51 in main.java

--- src/Main.java
+++ src/Main.java
@@ -48,7 +48,9 @@ input.close();
 			return false;
 		}
 		for(int i = 2; i*i <= num; i++) {
-			if(num % i == 0)return false;
+			if(num % i == 0) {
+				return false;
+			}
 		}
 		return true; 
 	}
