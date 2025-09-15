f6b5f9807b2907e3fdc2d7ea148a467c79d7068d
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-09-15 10:45:35 -0400

    Add files via upload

---
+++ DataRequest.java
@@ -0,0 +1,15 @@
+package datastore;
+
+public class DataRequest {
+    
+    private ColorWheelOption option;
+
+    // String of data
+    private byte[] dataRequest;
+
+    public DataRequest(byte[] dataRequest) {
+        this.dataRequest = dataRequest;
+    }
+    
+    boolean success()return true;
+}
