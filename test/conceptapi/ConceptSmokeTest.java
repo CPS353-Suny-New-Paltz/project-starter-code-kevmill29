5ac688be581c063527a33454218268032af83704
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-09-28 00:31:05 -0400

    Adding tests

---
+++ test/conceptapi/ConceptSmokeTest.java
@@ -0,0 +1,21 @@
+package conceptapi;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import networkapi.NetworkPrototype;
+
+public class ConceptSmokeTest {
+	@Test
+	void conceptSmokeTest() {
+		ConceptAPI mockComponent = mock(ConceptAPI.class);
+		
+		//assertion 
+		assertDoesNotThrow(() ->{
+			ConceptAPIPrototype prototype = new ConceptAPIPrototype();
+			prototype.prototype(mockComponent);
+		});
+	}
+}
---
+++ test/integration/ImplementDataStore.java
@@ -0,0 +1,37 @@
+package integration;
+
+import java.util.List;
+
+import processapi.DataStore;
+import processapi.InputConfig;
+import processapi.OutputConfig;
+import processapi.WriteData;
+
+public class ImplementDataStore implements DataStore {
+private final InputConfig<Integer> inputConfig;
+private final OutputConfig<String> outputConfig;
+
+public ImplementDataStore(InputConfig<Integer> inputConfig, OutputConfig<String> outputConfig) {
+	super();
+	this.inputConfig = inputConfig;
+	this.outputConfig = outputConfig;
+}
+	@Override
+	public WriteData writer(List<Integer> data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> input() {
+		// TODO Auto-generated method stub
+		return inputConfig.getInput();
+	}
+	public InputConfig<Integer> getInputConfig() {
+		return inputConfig;
+	}
+	
+	public void output(List<Integer>data){
+		outputConfig.writer(data);
+	}
+}
