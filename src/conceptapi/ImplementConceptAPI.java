3e9f6ca84b5c8bf561807ed3896a54300b4ebbef
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-09-30 18:36:28 -0400

    Delete src directory

--- src/conceptapi/ImplementConceptAPI.java
+++
@@ -1,9 +0,0 @@
-package conceptapi;
-
-public class ImplementConceptAPI implements ConceptAPI {
-
-	@Override
-	public ComputeComponent compute(UserComponent user) {
-        return new ComputeValues(); // or any other real ComputeComponent implementation
-    }
-}
