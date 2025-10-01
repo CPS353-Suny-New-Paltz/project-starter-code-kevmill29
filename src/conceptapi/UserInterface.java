427866e62e43b210210302c487ee8398b3febaae
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-10-01 16:46:33 -0400

    Delete src directory

--- src/conceptapi/UserInterface.java
+++
@@ -1,8 +0,0 @@
-package conceptapi;
-
-import java.io.IOException;
-import java.util.HashMap;
-
-public interface UserInterface {
-HashMap<Integer, String>getValues(int jobID) throws IOException;
-}
--- src/processapi/ProcessorAPI.java
+++
@@ -1,8 +0,0 @@
-package processapi;
-import assets.UserInputHandler;
-import project.annotations.ProcessAPI;
-@ProcessAPI
-public interface ProcessorAPI {
-ConvertData read(UserInputHandler handler);
-
-}
