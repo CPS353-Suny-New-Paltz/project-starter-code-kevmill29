93090629a5f13155a593d7b561e33dac2ed7b439
kevmill29 <kev29@fanaticalkev.com>
2025-11-03 17:55:54 -0500

    updating empty implementation

--- src/processapi/ImplementProcessorAPI.java
+++ src/processapi/ImplementProcessorAPI.java
@@ -1,28 +1,27 @@
 package processapi;
 
+import java.io.IOException;
 import java.util.List;
-
-import assets.UserInputHandler;
-import assets.UserRequest;
 
 public class ImplementProcessorAPI implements ProcessorAPI {
 
-	@Override
-	public ConvertData read(UserRequest input) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WriteData write(UserRequest output) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public List<Integer> input() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Integer> read(String filePath) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write(String output, List<Integer> data, String delimiter) throws IOException {
+		// TODO Auto-generated method stub
+		
 	}
 
 
