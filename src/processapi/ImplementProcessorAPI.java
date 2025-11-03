3f6007656a0c0191f9927fc83b2dabda304a9380
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-11-03 17:32:59 -0500

    reverting back to original

--- src/processapi/ImplementProcessorAPI.java
+++ src/processapi/ImplementProcessorAPI.java
@@ -1,46 +1,28 @@
 package processapi;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import assets.UserInputHandler;
+import assets.UserRequest;
 
 public class ImplementProcessorAPI implements ProcessorAPI {
 
-
 	@Override
-	public List<Integer> input() {
+	public ConvertData read(UserRequest input) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	//this method reads all the lines on the input file specified by the user
-	public List<Integer> read(String filePath) throws IOException {
-		Path path = Paths.get(filePath);
-		if(!Files.exists(path)) {
-			throw new FileNotFoundException("File not found: " +filePath);
-		}
-		return Files.readAllLines(path).stream()//streams through the strings within the file
-				.map(String::trim) // shortens the string if there are empty spaces in the line
-				.filter(s->!s.isEmpty())// checks if line is empty and will skip if the line is not
-				.map(Integer::parseInt)// converts String into integer
-				.collect(Collectors.toList()); // creates new list and collects all found integers
+	public WriteData write(UserRequest output) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	//this method will write the data from the given equation into a file with the specified location
-	public void write(String output, List<Integer> data, String delimiter) throws IOException {
-		//this part will join the integers to a single line
-		String line = data.stream()
-				.map(Object::toString)
-				.collect(Collectors.joining(delimiter));
-		//this part will write the file
-		Files.write(Paths.get(output), List.of(line));
-		
+	public List<Integer> input() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
