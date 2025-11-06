0d0b702f686b96393229a4ad8ad77f77b20fb3dd
kevmill29 <kev29@fanaticalkev.com>
2025-11-06 10:22:41 -0500

    Adding try catch to prototype

--- src/networkapi/NetworkPrototype.java
+++ src/networkapi/NetworkPrototype.java
@@ -1,7 +1,6 @@
 package networkapi;
 
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +22,7 @@ import project.annotations.NetworkAPIPrototype;
 		List<Integer>results = new ArrayList<>();
 		//Network API implementation
 		api = new ImplementNetworkAPI();
-		
+		try {
 		if(request.validation()== UserRequestCode.SUCCESS_RESPONSE) {
 			//Call the storage component
 			ProcessorAPI storage = new ImplementProcessorAPI();
@@ -36,6 +35,10 @@ import project.annotations.NetworkAPIPrototype;
 			//the respond method uses the init method to check if request is valid again to ensure validity
 			// compute component performs computation and storage component reads through file in request to pass to computecomponent
 		
+		}
+		}catch(Exception e) {
+			System.err.println("Could not complete process please try again!");
+			throw new RuntimeException("Process failed!"+ e.getMessage());
 		}
 		System.out.println(request.validation());
 
