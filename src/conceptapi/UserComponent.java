c10ba405d6d0c53ed2f3b4f9bd8aa31ba5138e2d
kevmill29 <kev29@fanaticalkev.com>
2025-11-05 22:29:58 -0500

    updating validation test to add error messages for exceptions

--- src/conceptapi/UserComponent.java
+++ src/conceptapi/UserComponent.java
@@ -5,7 +5,7 @@ public class UserComponent implements UserInterface{
 	
 	public boolean checkValue(int valueA) {
 
-		return valueA> 1000 || valueA < -1000;
+		return valueA > 1000 || valueA < -1000;
 			
 	}
 }
--- test/smoketest/ValidationTest.java
+++ test/smoketest/ValidationTest.java
@@ -3,8 +3,6 @@ package smoketest;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +11,7 @@ import assets.InvalidValueException;
 import assets.UserRequest;
 import conceptapi.ComputeComponent;
 import conceptapi.ImplementConceptAPI;
+import conceptapi.UserComponent;
 import networkapi.ImplementNetworkAPI;
 import networkapi.NetworkInterfaceAPI;
 import processapi.ImplementProcessorAPI;
@@ -21,15 +20,17 @@ import processapi.ProcessorAPI;
 public class ValidationTest {
 @Test
 public void computeValidationTest() {
+	UserComponent validator = new UserComponent();
 	ComputeComponent mockComponent = new ImplementConceptAPI();
-	int valueA = 10000;// value above 10000
-	assertThrows(InvalidValueException.class, () -> mockComponent.computeValue(valueA)); //check if computeValues throws the exception
+	int valueA = -1001;// value above 10000
+	assertThrows(InvalidValueException.class, () -> mockComponent.computeValue(valueA), "If value is above 1000 or below -1000 it should throw an exception." 
+	+"Validation check on: " +valueA + " Result: " +validator.checkValue(valueA)); //check if computeValues throws the exception
 }
 @Test
 public void storageValidationTest() {
 	ProcessorAPI mockStorage = new ImplementProcessorAPI();
 	String mockInput = "well..."; //false address should throw an file exception for when the component tries to read
-	assertThrows(FileNotFoundException.class, ()-> mockStorage.read(mockInput));
+	assertThrows(FileNotFoundException.class, ()-> mockStorage.read(mockInput), "Because input location string isn't actually pointing to a location should throw an exception"+ " String: "+mockInput);
 }
 
 @Test
@@ -37,7 +38,7 @@ public void coordinationValidationTest() {
 	ProcessorAPI mockStorage = new ImplementProcessorAPI();
 	NetworkInterfaceAPI mockAPI = new ImplementNetworkAPI();
 	UserRequest request = new UserRequest(null, null, null);
-	assertThrows(InvalidRequestException.class, ()-> mockAPI.readRequest(mockStorage, request));
+	assertThrows(InvalidRequestException.class, ()-> mockAPI.readRequest(mockStorage, request), "Because request is null should throw an exception" +request);
 	
 }
 
