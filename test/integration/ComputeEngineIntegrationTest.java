b3ee3a6295b73f0bc5d7776f3886aac961a088ad
kevmill29 <kev29@fanaticalkev.com>
2025-11-06 13:14:58 -0500

    Adding Integration Test for Exception Handling

--- test/integration/ComputeEngineIntegrationTest.java
+++ test/integration/ComputeEngineIntegrationTest.java
@@ -1,6 +1,9 @@
 package integration;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,7 +12,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import assets.UserRequest;
-import assets.UserRequestCode;
 import conceptapi.ComputeComponent;
 import conceptapi.ImplementConceptAPI;
 import networkapi.ImplementNetworkAPI;
@@ -43,7 +45,7 @@ public class ComputeEngineIntegrationTest {
         List<Integer> data = datastore.input();
         for (int i = 0; i < data.size(); i++) {
             int valueA = inputData.get(i);
-            int result = concept.computeValue(valueA); // if the enum is giving a true response it will use the implemented class conceptAPI method to calculate the answer
+            int result = userInterface.respond(isInit,valueA, concept); // if the enum is giving a true response it will use the implemented class conceptAPI method to calculate the answer
             //if the conceptAPI is working properly and the network API is started up properly then it will perform the calculation and then will be added to the results list
             results.add(result);
             output.write(Integer.toString(result));
@@ -58,6 +60,22 @@ public class ComputeEngineIntegrationTest {
 
         assertEquals(expected, output.getOutput());
     }
+    
+    //test if exception handling is working 
+  @Test
+  public void shouldThrowTest() {
+	  NetworkInterfaceAPI testAPI = new ImplementNetworkAPI();
+	  ComputeComponent testCompute = new ImplementConceptAPI();
+	  List<Integer> data = Arrays.asList(1,10,25);
+	  boolean isInit = false; // to force an error
+	  List<Integer> results = testAPI.respond(isInit, testCompute, data);
+	  //test 1
+	  assertNotNull(results);
+	  
+	  assertTrue(results.isEmpty(), "Expected empty list instead of an exception being thrown");
+	  
+  }
+
 
    
 }
