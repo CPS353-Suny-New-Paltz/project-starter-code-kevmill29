0a29423e9b04a7ca96cf0eff459b7801c6c4a83f
kevmill29 <kev29@fanaticalkev.com>
2025-11-11 13:41:15 -0500

    Fixing import
    Eclipse imported junit4 version of assertNull but junit5 is in the build instead of 4 causing pipeline build failure

--- test/smoketest/ValidationTest.java
+++ test/smoketest/ValidationTest.java
@@ -1,6 +1,6 @@
 package smoketest;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
