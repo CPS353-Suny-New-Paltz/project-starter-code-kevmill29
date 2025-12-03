3a6115d72794576ad023c8f01dee74a5a5bd0efb
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-12-03 18:08:19 -0500

    Refactor NetworkInterfaceAPI methods and parameters

--- src/networkapi/NetworkInterfaceAPI.java
+++ src/networkapi/NetworkInterfaceAPI.java
@@ -11,14 +11,15 @@ import project.annotations.NetworkAPI;
 
 @NetworkAPI
 public interface NetworkInterfaceAPI {
-	List<Integer> respond(boolean isInit, UserRequest request);
+	List<Integer> respond(String input, String output, char delimiter);
 	boolean initialize(UserRequest request);
 	List<String> readRequest( UserRequest request);
 	int respond(boolean isInit, int valueA, ComputeComponent concept) ; //using for integration test
-	void writeRequest(List<Integer> results, UserRequest request) ;
 	void shutdown();
 	UserRequest buildRequest(String input, String output,char delimiter);
-	
-	
+	void writeRequest(List<Integer> newData, UserRequest request);
+
+
 }
+
 
